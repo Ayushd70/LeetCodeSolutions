415ef59c4d95c428b0c0becadb574e9c19294465
dracarys18 <karthihegde010@gmail.com>
2020-10-01 14:43:39 +0530

    CPP: Palindrome number Solution
    Signed-off-by: dracarys18 <karthihegde010@gmail.com>

---
+++ CPP/Palindrome.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int rev=0,tmp=0;
+        while(x!=0)
+        {
+            tmp=x%10;
+            x=x/10;
+            /*overflow check*/
+            if (rev > INT_MAX/10 || (rev == INT_MAX / 10 && tmp > 7)) return 0;
+            if (rev < INT_MIN/10 || (rev == INT_MIN / 10 && tmp < -8)) return 0;
+            rev=(rev*10)+tmp;
+        }
+        return rev;
+    }
+
+    bool isPalindrome(int x) {
+        if(x<0)
+            return false;
+        int rev = reverse(x);
+        return x==rev?true:false;
+    }
+};
