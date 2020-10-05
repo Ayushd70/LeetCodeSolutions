89a3c0840963f8a7465300df76d9b1dd63647f86
Phantom <72387813+supboom@users.noreply.github.com>
2020-10-05 17:10:50 +0530

    Created Add Digits
    258. Add Digits
    
    Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.

---
+++ CPP/addDigit.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int addDigits(int n) {
+         int sum = 0; 
+    while(n > 0 || sum > 9) 
+    { 
+        if(n == 0) 
+        { 
+            n = sum; 
+            sum = 0; 
+        } 
+        sum += n % 10; 
+        n /= 10; 
+    } 
+    return sum; 
+    }
+};
