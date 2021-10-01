6e720431eaca8e2457d4e4039dabbf4d50741c26
Arpit Choudhary <64888271+ArpitMihawkZoro@users.noreply.github.com>
2021-10-01 19:40:25 +0530

    Python: Rotate Image
    Link: https://leetcode.com/problems/rotate-image/

---
+++ Python/Rotate_Image.py
@@ -0,0 +1,12 @@
+def rotate(matrix: list[list[int]]) -> None:
+
+    for i in range(len(matrix)):
+        for j in range(i, len(matrix)):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    for i in range(len(matrix)):
+        left = 0
+        right = len(matrix) - 1
+        while left < right:
+            matrix[i][left], matrix[i][right] = matrix[i][right], matrix[i][left]
+            left += 1
+            right -= 1
