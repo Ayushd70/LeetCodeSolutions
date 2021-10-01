ced006055afa1ddc3d507b7610e1a4db4cad2094
Himanshu Kharkar <himanshu.kharkar20@gmail.com>
2021-10-01 15:13:23 +0530

    JavaScript: solution for reverse integer
    Challenge link :- https://leetcode.com/problems/reverse-integer/
    
    Signed-off-by: Himanshu Kharkar <himanshu.kharkar20@gmail.com>

---
+++ JavaScript/reverse-integer.js
@@ -0,0 +1,16 @@
+var reverse = function (x) {
+    let isNegative = false;
+    if (x < 0) {
+        isNegative = true;
+        x = -x;
+    }
+    let reverse = 0;
+    while (x > 0) {
+        reverse = reverse * 10 + x % 10;
+        x = parseInt(x / 10);
+    }
+    if (reverse >= Math.pow(2, 31) - 1 || reverse <= Math.pow(-2, 31)) {
+        return 0;
+    }
+    return isNegative ? -reverse : reverse;
+};
