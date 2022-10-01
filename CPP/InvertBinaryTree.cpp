150034ed04a25b774e1e143fa52ace00d7a5e08c
glitch <52325315+SamyakJain222@users.noreply.github.com>
2022-10-01 17:51:29 +0530

    CPP: Invert Binary Tree
    problem: https://leetcode.com/problems/invert-binary-tree/

---
+++ CPP/InvertBinaryTree.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == NULL) return root;
+        swap(root->left, root->right);
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;
+    }
+};
