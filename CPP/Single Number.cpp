f0203a8ada6db1cd33b60e2adcabfa4a45288ade
Vaibhav Dalakoti <38762713+vaibd@users.noreply.github.com>
2022-10-22 10:55:27 +0530

    Problem link: https://leetcode.com/problems/single-number/

---
+++ CPP/Single Number.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+	public:
+		int singleNumber(vector<int> &nums)
+		{
+			//initialised the xor of numbers in xorr variable
+			int xorr = 0;
+			//Linearly traversing the array
+			for (auto x = 0; x < nums.size(); x++)
+			{
+				//Calculating Xor of two numbers .
+				xorr = xorr ^ nums[x];
+
+			}
+
+			// Atlast return the final xor .
+
+			return xorr;
+		}
+};
